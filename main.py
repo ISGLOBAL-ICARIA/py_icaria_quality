5cb4d7794f7961ffe90d42455521ecf601294030
andreubp <andreu.bp@gmail.com>
2024-03-15 09:24:29 +0100

    Adding main and params

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+""" Python script to manage pontential ghost switches between REDCap projects.
+Part of the ICARIA Clinical Trial"""
+
+import quality
+
+__author__ = "Andreu Bofill Pumarola"
+__copyright__ = "Copyright 2024, ISGlobal Maternal, Child and Reproductive Health"
+__credits__ = ["Andreu Bofill Pumarola"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__date__ = "20240315"
+__maintainer__ = "Andreu Bofill"
+__email__ = "andreu.bofill@isglobal.org"
+__status__ = "Dev"
+
+
+if __name__ == '__main__':
+    quality.()
